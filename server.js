5e1a9cb4f865aaab8a677e60f360fbc1f09c7a1b
Brookbayu2 <babebe@4myseneca.ca>
2024-07-04 22:07:31 -0400

    Setup initial project structure

--- server.js
+++ server.js
@@ -12,11 +12,11 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
     res.redirect('/about');
 });
-
+//////////////////////////////////////////////////////////////////////
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
-});
+});///
 
 
 app.get('/shop', (req, res) => {
